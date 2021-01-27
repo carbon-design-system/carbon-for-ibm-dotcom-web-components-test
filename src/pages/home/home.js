ffb68cee0111981d70d73faaf6b3875a4b8112a3
IgnacioBecerra <i1becerr@ucsd.edu>
2021-01-26 18:01:39 -0800

    feat(components-test): recreated pages

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,4 +1,5 @@
 // TODO: separate chunks loaded do not seem to render for some reason, need to investigate this
-import '@carbon/ibmdotcom-web-components/es/components/card-group/card-group';
-import '@carbon/ibmdotcom-web-components/es/components/card-group/card-group-item';
-import '@carbon/ibmdotcom-web-components/es/components/card/card-footer';
+import '@carbon/ibmdotcom-web-components/es/components/content-section/content-section-heading.js';
+import '@carbon/ibmdotcom-web-components/es/components/card-section-simple/card-section-simple.js';
+import '@carbon/ibmdotcom-web-components/es/components/card-group/card-group.js';
+import '@carbon/ibmdotcom-web-components/es/components/card-group/card-group-item.js';
--- src/pages/ui-components/ui-components.js
+++ src/pages/ui-components/ui-components.js
@@ -1,6 +1,19 @@
 // TODO: separate chunks loaded do not seem to render for some reason, need to investigate this
 
-import '@carbon/ibmdotcom-web-components/es/components/button-group/button-group';
-import '@carbon/ibmdotcom-web-components/es/components/button-group/button-group-item';
-import '@carbon/ibmdotcom-web-components/es/components/feature-card-block-large/feature-card-block-large';
-import '@carbon/ibmdotcom-web-components/es/components/image/image';
+import '@carbon/ibmdotcom-web-components/es/components/button-group/button-group.js';
+import '@carbon/ibmdotcom-web-components/es/components/button-group/button-group-item.js';
+import '@carbon/ibmdotcom-web-components/es/components/card-group/card-group.js';
+import '@carbon/ibmdotcom-web-components/es/components/card-group/card-group-item.js';
+import '@carbon/ibmdotcom-web-components/es/components/card/card-footer.js';
+import '@carbon/ibmdotcom-web-components/es/components/card/card.js';
+import '@carbon/ibmdotcom-web-components/es/components/card-link/card-link.js';
+import '@carbon/ibmdotcom-web-components/es/components/horizontal-rule/horizontal-rule.js';
+import '@carbon/ibmdotcom-web-components/es/components/feature-card-block-medium/feature-card-block-medium.js';
+import '@carbon/ibmdotcom-web-components/es/components/feature-card-block-large/feature-card-block-large.js';
+import '@carbon/ibmdotcom-web-components/es/components/image/image.js';
+import '@carbon/ibmdotcom-web-components/es/components/cta/text-cta.js';
+import '@carbon/ibmdotcom-web-components/es/components/cta/video-cta-container.js';
+import '@carbon/ibmdotcom-web-components/es/components/link-list/link-list.js';
+import '@carbon/ibmdotcom-web-components/es/components/link-list/link-list-item-card.js';
+import '@carbon/ibmdotcom-web-components/es/components/quote/quote';
+import '@carbon/ibmdotcom-web-components/es/components/link-with-icon/link-with-icon';

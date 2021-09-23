8fb6ffd89d8aaa9be71b789d772dd8f9cda693e1
Jeff Chew <jeff.chew@gmail.com>
2021-09-23 11:32:43 -0400

    test(e2e): fix to the test links

--- tests/e2e/__tests__/example-page-c/example-page-c.e2e.js
+++ tests/e2e/__tests__/example-page-c/example-page-c.e2e.js
@@ -9,7 +9,7 @@
 
 describe('Example page C page', () => {
   it('should load the default example-page-c page', () => {
-    cy.visit('/example-page-c.html');
+    cy.visit('/example-page-c/index.html');
 
     // Take a snapshot for visual diffing
     cy.percySnapshot('example page c | default');

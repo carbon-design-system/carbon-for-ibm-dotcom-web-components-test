d351995839c606ac0b11108308a12971287cb5ba
Putra Roeung Bonaccorsi <putra.bonaccorsi@lullabot.com>
2021-09-27 11:00:41 -0400

    feat(add-cloud-masthead): add cloud test page (#34)
    ### Related Ticket(s)
    
    N/A
    
    ### Description
    
    - This PR adds a test page that showcases the <dds-cloud-masthead-container> component.
    
    ### Changelog
    
    **New**
    
    - cloud-base.hbs - base handlebar template that doesn't depend on `<dds-dotcom-shell-container>` component.
    
    **Changed**
    
    - {{changed thing}}
    
    **Removed**
    
    - {{removed thing}}

--- src/pages.js
+++ src/pages.js
@@ -59,6 +59,18 @@ const pages = [
     },
     template: './src/pages/services/services.hbs',
   },
+  {
+    output: './cloud/index.html',
+    chunks: ['cloud'],
+    content: {
+      title: 'Cloud template',
+      description: 'Cloud template',
+    },
+    chunkEntry: {
+      cloud: './src/pages/cloud/cloud.js',
+    },
+    template: './src/pages/cloud/cloud.hbs',
+  },
 ];
 
 module.exports = pages;
